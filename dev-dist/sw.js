7d87c86338764357088b3d4908ac812a08d6baef
Paulo Conde <pauloconde@gmail.com>
2026-06-10 20:10:13 -0400

    fix orden eventos

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/",
-    "revision": "0.ono0p7d3q3"
+    "revision": "0.qmsadlcfk5o"
   }], {
     "directoryIndex": "index.html"
   });
